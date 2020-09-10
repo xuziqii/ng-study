8c75c52c0c941a7017e22f12be80abb1d2a82b76
xuziqii <xuclour@foxmail.com>
2020-09-10 11:01:32 +0800

    update: 使用 @HostBinding @Input 设置宿主样式

--- src/app/share/directives/gird-item.directive.ts
+++ src/app/share/directives/gird-item.directive.ts
@@ -1,16 +1,11 @@
-import { Directive, Renderer2, ElementRef, OnInit, HostBinding } from '@angular/core'
+import { Directive, HostBinding, Input } from '@angular/core'
 
 @Directive({
   selector: '[appGirdItem]'
 })
-export class GirdItemDirective implements OnInit {
-  constructor (private elr: ElementRef, private renderer2: Renderer2) {
-  }
-  ngOnInit(): void {
-    this.renderer2.setStyle(this.elr.nativeElement, 'display', 'grid')
-    this.renderer2.setStyle(this.elr.nativeElement, 'grid-template-areas', `'image' 'title'`)
-    this.renderer2.setStyle(this.elr.nativeElement, 'place-items', 'center')
-    this.renderer2.setStyle(this.elr.nativeElement, 'width', '4rem')
-    this.renderer2.setStyle(this.elr.nativeElement, 'background-color', 'green')
-  }
+export class GirdItemDirective {
+  @HostBinding('style.display') display = 'grid'
+  @HostBinding('style.grid-template-areas') templateArea = `'image' 'title'`
+  @HostBinding('style.place-items') @Input() placeItems = 'center'
+  @HostBinding('style.background-color') bgColor = 'green'
 }
